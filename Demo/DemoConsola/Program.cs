b813835f4dacb0cfb964a74243cbfa4253c59f1b
Noel Vaca <noel.vm.2501@gmail.com>
2026-03-07 09:24:19 -0400

    Inicio del repositorio de la materia

---
+++ Demo/DemoConsola/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// Esto es un comentario de una sola línea
+/* Esto es un comentario
+   de varias líneas */
+
+// Variables
+var variable = "Cualquier valor";
+bool esActivo = false;
+short enteroCorto = 12;
+int entero = 123;
+long enteroLargo = 1234567890;
+double real = 3.1416;
+decimal real2 = 3.1416m;
+float real3 = 3.1416f;
+char caracter = 'A';
+string cadena = "Esto es una cadena";
+Int16 enteroCorto2 = 12; // short
+Int32 entero2 = 123; // int
+Int64 enteroLargo2 = 1234567890; // long
+DateTime fecha = DateTime.Now;
+DateTime fecha2 = new DateTime(2026, 3, 2);
+
+// Constantes
+const double PI = 3.1416;
+const string SIGLA_MATERIA = "SIS457";
+
+// Operadores y expresiones
+entero = 33;
+entero2 += 10;
+entero2 %= 2;
+bool esPar = entero2 % 2 == 0;
+int a = 5 + 7 * 2;
+int b = (5 + 7) * 2;
+b++;
+
+Console.WriteLine(a);
